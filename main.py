ac8bebf20bd3742e4b21727c2de208c87f27c328
Zaw Ye Htet <zawye.htet5@frontiir.net>
2023-12-24 13:52:22 +0630

    constructing routes for first time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Path, Query
 from typing import Optional, List
 from pydantic import BaseModel
+from api import user, course, section
 
 app = FastAPI(
     title="FastApi Crash Course",
@@ -15,25 +16,6 @@ app = FastAPI(
     },
 )
 
-users = []
-
-class User(BaseModel):
-    email:    str
-    is_active:  bool
-    bio:        Optional[str]
-
-@app.get('/users', response_model=List[User])
-async def get_users():
-    return users
-
-@app.post('/users')
-async def create_user(user: User):
-    users.append(user)
-    return "Success"
-
-@app.get('/users/{id}')
-async def get_user(
-    id:     int = Path(..., description="The ID of the user you want to retrieve.", gt=2),
-    q:      str = Query(None, max_length=5)
-):
-    return {"user": users[id], "query": q}
+app.include_router(user.router)
+app.include_router(course.router)
+app.include_router(section.router)
